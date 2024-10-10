7fb2f459a4202a0076cea4bf9b89d7b92a6ff928
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-10-10 08:42:25 +0300

    initial commit

---
+++ src/test_htmlnode.py
@@ -0,0 +1,20 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLtNode(unittest.TestCase):
+    def test_2(self):
+        node = HTMLNode()        
+        self.assertEqual(node.tag, node.tag)
+
+    def test_0(self):
+        node = HTMLNode()        
+        self.assertEqual(node.tag, node.tag)
+
+    def test_1(self):
+        node = HTMLNode()        
+        self.assertEqual(node.tag, node.tag)
+
+if __name__ == "__main__":
+    unittest.main()
